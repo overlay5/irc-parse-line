56d93664b6832b7f1732a2ab79bbb331ce87c623
Evgeny Zislis <evgeny.zislis@gmail.com>
2020-05-09 09:16:39 +0000

    implement parsing of irc message tags and prefix

---
+++ index.js
@@ -0,0 +1,67 @@
+// Parser for IRC protocol messages RFC 2812 + IRCv3 message-tag extension
+// (c) 2020 Evgeny Zislis <evgeny.zislis@gmail.com>
+// Released under the MIT License
+// ** https://tools.ietf.org/html/rfc2812
+// ** https://ircv3.net/specs/extensions/message-tags.html
+
+// Based on:
+// https://github.com/grawity/code/blob/master/lib/python/nullroute/irc/parse.py
+// Parser for IRC protocol messages (RFC 1459 + IRCv3 message-tag extension)
+// (c) 2012-2014 Mantas Mikulėnas <grawity@gmail.com>
+// Released under the MIT License (dist/LICENSE.mit)
+
+class InvalidMessage extends Error {}
+
+/**
+ * Parsing a single line IRC message
+ * @param {string} message the irc message to parse
+ */
+function parse(message) {
+  if (!message)
+    throw new InvalidMessage
+
+  let pos = 0
+  const parsed = {}
+
+  if (message[pos] === '@') {
+    pos++ // '@' at the start of tags
+    const tags_end = message.indexOf(' ', pos)
+    parsed.tags = Object.fromEntries(
+      message.slice(pos, tags_end)
+      .split(';')
+      .map(tag => {
+        [k,v] = tag.split('=')
+        return [k, typeof v === 'undefined' ? true : v]
+      })
+    )
+    pos = tags_end + 1
+  }
+
+  if (message[pos] === ':') {
+    pos++ // ':' at the start of prefix
+    const prefix_end = message.indexOf(' ', pos)
+    const prefix = message.slice(pos, prefix_end)
+    const dot_pos = prefix.indexOf('.')
+    const user_pos = prefix.indexOf('!')
+    const host_pos = prefix.indexOf('@', user_pos-1)
+    if (user_pos !== -1 && host_pos !== -1) {
+      parsed.nickname = prefix.slice(0,user_pos)
+      parsed.user = prefix.slice(user_pos+1,host_pos)
+      parsed.host = prefix.slice(host_pos+1)
+    } else if (host_pos !== -1) {
+      parsed.nickname = prefix.slice(0,host_pos)
+      parsed.host = prefix.slice(host_pos+1)
+    } else if (dot_pos !== -1) {
+      parsed.servername = prefix
+    } else {
+      parsed.nickname = prefix
+    }
+  }
+
+  return parsed
+}
+
+module.exports = {
+  parse,
+  InvalidMessage
+}
---
+++ test/index.test.js
@@ -0,0 +1,51 @@
+const assert = require('assert').strict
+const { parse, InvalidMessage } = require('../index')
+
+describe('message prefix', function () {
+  it('should throw an error on invalid messages', function () {
+    assert.throws(function () { parse()    }, InvalidMessage)
+    assert.throws(function () { parse('')  }, InvalidMessage)
+    // assert.throws(function () { parse(4)   }, InvalidMessage)
+    // assert.throws(function () { parse([])  }, InvalidMessage)
+    // assert.throws(function () { parse({})  }, InvalidMessage)
+    // assert.throws(function () { parse('JOIN') }, InvalidMessage)
+  })
+
+  it('should parse a prefix with a servername', function () {
+    const result = parse(':tmi.twitch.tv CAP * ACK :twitch.tv/tags')
+    assert.equal(result.servername, 'tmi.twitch.tv')
+  })
+
+  it('should parse a prefix with a full nickname', function () {
+    const result = parse(':nightbot!botnight@nightbot.tmi.twitch.tv JOIN #kesor6')
+    assert.equal(result.nickname, 'nightbot')
+    assert.equal(result.user,     'botnight')
+    assert.equal(result.host,     'nightbot.tmi.twitch.tv')
+  })
+
+  it('should parse a prefix with a partial nickname', function () {
+    const result = parse(':nightbot@nightbot.tmi.twitch.tv JOIN #kesor6')
+    assert.equal(result.nickname, 'nightbot')
+    assert.equal(result.host,     'nightbot.tmi.twitch.tv')
+  })
+
+  it('should parse a prefix with a short nickname', function () {
+    const result = parse(':nightbot JOIN #kesor6')
+    assert.equal(result.nickname, 'nightbot')
+  })
+
+  it('should parse tags before the message', function () {
+    const result = parse('@enabled-tag;color=#0000FF;display-name=kesor6 :tmi.twitch.tv GLOBALUSERSTATE')
+    assert.deepEqual(result.tags, {
+      'enabled-tag': true,
+      'color': '#0000FF',
+      'display-name': 'kesor6',
+    })
+  })
+
+  xit('should parse GLOBALUSERSTATE commands', function () {
+    const result = parse(':tmi.twitch.tv GLOBALUSERSTATE')
+    assert.deepEqual(result.command, 'GLOBALUSERSTATE')
+  })
+
+})
